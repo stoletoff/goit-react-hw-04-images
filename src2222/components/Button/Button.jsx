79642615d2ff92394080b213aa4587a8f500037b
Alex <afanasbev1@gmail.com>
2023-06-19 13:25:23 +0300

    upd

--- src2222/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { Btn } from './Button.styled';
-
-export const Button = ({ onMoreLoad }) => {
-  return (
-    <Btn onClick={onMoreLoad} type="button">
-      Load more
-    </Btn>
-  );
-};
